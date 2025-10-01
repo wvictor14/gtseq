bec9383970006b89e9a1bf64132e2504b2554f12
Victor Yuan <victor.2wy@gmail.com>
2025-09-30 17:01:43 -0700

    add breaks

---
+++ R/breaks.R
@@ -0,0 +1,18 @@
+#' takes a vector as input, generates breaks every n times
+#'
+#' Give the table's position columns, treat as a numerical axis, and generates breaks using `scales::breaks_width`
+#' @param x a vector of column names
+#' @return a vector of column names at every `width` position
+#' @export
+#'
+generate_breaks <- function(cnames, width = 5) {
+  n_col <- cnames |> ncol()
+
+  breaks <- scales::breaks_width(width, 0)(c(1, n_col))
+
+  # handle case where breaks extend beyond data
+  breaks <- intersect(breaks, 1:n_col)
+
+  col_breaks <- colnames(cnames[, breaks])
+  return(col_breaks)
+}
