75fe6e23e56076719a25e5f0e28c27674343188d
Victor Yuan <victor.2wy@gmail.com>
2025-10-02 19:51:32 -0700

    dev breaks

--- R/breaks.R
+++ R/breaks.R
@@ -4,15 +4,23 @@
 #' @param x a vector of column names
 #' @return a vector of column names at every `width` position
 #' @export
+#' @examples
 #'
+#' library(dplyr)
+#' cnames <- msa |>
+#'   split_sequences(seq_column = seq) |>
+#'   select(contains("pos_")) |>
+#'   colnames()
+#' generate_breaks(cnames)
 generate_breaks <- function(cnames, width = 5) {
-  n_col <- cnames |> ncol()
+  n_col <- cnames |> length()
 
+  browser()
   breaks <- scales::breaks_width(width, 0)(c(1, n_col))
 
   # handle case where breaks extend beyond data
   breaks <- intersect(breaks, 1:n_col)
 
-  col_breaks <- colnames(cnames[, breaks])
+  col_breaks <- colnames(cnames[breaks])
   return(col_breaks)
 }
